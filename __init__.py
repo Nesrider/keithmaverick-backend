92ac35ba84141226b201291669ab7dca0b8468da
Keith Yeung <kmyeung@andrew.cmu.edu>
2017-01-07 04:35:49 -0500

    JSON Conversion

--- __init__.py
+++ __init__.py
@@ -1,16 +1,34 @@
-from flask import Flask,request
+from flask import Flask,request, jsonify
 from dbconnect import connection
 import gc
 import json
 
 app = Flask(__name__)
 
-def toJSON(sqlData, sqlHeader):
+#Takes the returned sql query and converts
+#it into a dictionary format
+def toDict(sqlData, Header, table_name):
 
-	for i in xrange(len(sqlObject)):
-		for j in xrange(len(sqlObject[i])):
+	rows = []
 
-			return 0
+	for i in xrange(len(sqlData)):
+		row ={}
+
+		for j in xrange(len(Header)):
+			row[Header[j]] = sqlData[i][j]
+
+		rows.append(row)
+
+	return {table_name : rows}
+
+#Takes a returned 
+def getHeaders(sqlHeader):
+	header = []
+
+	for i in xrange(len(sqlHeader)):
+		header.append(sqlHeader[i][0])
+
+	return header
 
 def query(statement):
 	try: 
@@ -27,14 +45,27 @@ def query(statement):
 	except Exception as e:
 		return(str(e))
 
+#Returns a JSON of a Table
+def getTable(table_name):
+	result = query("SELECT * FROM %s" % table_name)
+	resultHeader = getHeaders(query("SHOW COLUMNS FROM %s" % table_name))
+	return jsonify(toDict(result, resultHeader, table_name))
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/projects', methods=["GET"])
+def projects():
+	return getTable("PROJECT")
+
+@app.route('/images', methods=["GET"])
+def images():
+	return getTable("IMAGE")
 
 @app.route('/subjects', methods=["GET"])
 def subjects():
-	result = query("SELECT * FROM `SUBJECT`")
-	resultHeader = query("SELECT * FROM `INFORMATION_SCHEMA`.`COLUMNS`")
-	return str(result)
+	return getTable("SUBJECT")
+
+
+
