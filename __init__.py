c0eb65d1d0c88c8cc16456177ada8a704fdd4d40
Keith Yeung <kmyeung@andrew.cmu.edu>
2017-01-10 01:40:45 -0500

    updated connection

--- __init__.py
+++ __init__.py
@@ -30,16 +30,23 @@ def getHeaders(sqlHeader):
 
 	return header
 
-def query(statement):
-	try: 
+def openConnection():
+	try:
 		c, conn = connection()
+		return c, conn
+	except Exception as e:
+		return (str(e))
+
+def closeConnection(c, conn):
+	c.close()
+	conn.close()
+	gc.collect
+	return
+
+def query(c, statement):
+	try: 
 		allSubjects = c.execute(statement)
 		queryResult = c.fetchall()
-
-		c.close()
-		conn.close()
-		gc.collect()
-
 		return queryResult
 
 	except Exception as e:
@@ -47,13 +54,34 @@ def query(statement):
 
 #Returns a JSON of a Table
 def getTable(table_name):
-	result = query("SELECT * FROM %s" % table_name)
-	resultHeader = getHeaders(query("SHOW COLUMNS FROM %s" % table_name))
+	c, conn = openConnection()
+	result = query(c, "SELECT * FROM %s" % table_name)
+	resultHeader = getHeaders(
+		query(c, "SHOW COLUMNS FROM %s" % table_name))
+	closeConnection(c, conn)
+	return jsonify(toDict(result, resultHeader, table_name))
+
+
+def getById(table_name, id_name, id_value):
+	c, conn = openConnection()
+	result = query(c, "SELECT * FROM %s WHERE %s = %d" % 
+		(table_name, id_name, id_value))
+	resultHeader = getHeaders(
+		query(c, "SHOW COLUMNS FROM %s" % table_name))
+	closeConnection(c, conn)
 	return jsonify(toDict(result, resultHeader, table_name))
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Keith Maverick - Backend'
+
+@app.route('/projects/sub/<int:subject_id>', methods=["GET"])
+def projectsBySubject(subject_id):
+	return getById("PROJECT", "SUBJECT_ID", subject_id)
+
+@app.route('/images/sub/<int:subject_id>', methods=["GET"])
+def imagesBySubject(subject_id):
+	return getById("IMAGE", "SUBJECT_ID", subject_id)
 
 @app.route('/projects', methods=["GET"])
 def projects():
